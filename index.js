f9a3f60e477a4c2bffb5c2acd17fba8808e73927
rahulkumarv269 <rahulkumarv269@gmail.com>
2025-07-23 14:45:03 +0530

    added: vercel

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.LOCALHOST,
+    origin: [process.env.LOCALHOST,process.env.ORIGN],
     credentials: true,
   })
 );
